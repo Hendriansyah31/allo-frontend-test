1056102ad1509db949ca34ac1a9d5668711a36e0
hendri <hendriansyah3105@gmail.com>
2025-03-04 17:14:49 +0700

    done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-app-bar app color="secondary" light>
+      <div class="d-flex align-center">
+        <h2>SpaceX Launch</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <v-timeline v-if="launches.length > 0">
+          <LaunchTimlineItem
+            v-for="launch in launches"
+            :key="launch.flight_number"
+            :launch="launch"
+          />
+        </v-timeline>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
-<script lang="ts" setup>
-  //
+<script>
+import axios from "axios";
+import LaunchTimlineItem from "./components/LaunchTimeLineItem.vue";
+export default {
+  name: "App",
+  components: {
+    LaunchTimlineItem,
+  },
+  data: () => ({
+    launches: [],
+  }),
+  async created() {
+    const { data } = await axios.get(
+      "https://api.spacexdata.com/v3/launches/past"
+    );
+    
+    data.forEach((launch) => {
+      this.launches.push(launch);
+    });
+  },
+};
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <HelloWorld />
+  <LaunchTimeLineItem />
 </template>
 
 <script lang="ts" setup>
